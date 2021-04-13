0f4f9d3034430aaec7238d3e553ce535dc6c22a9
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 13:33:13 +0200

    chore: add code commenting

--- src/app/services/save-btn.service.ts
+++ src/app/services/save-btn.service.ts
@@ -8,10 +8,15 @@ export class SaveBtnService {
   constructor() { }
  
   saveBtnData(recipe: any){
+    // Parse localStorage target 'nomnombase'
     const clientStore = JSON.parse(localStorage.getItem('nomnombase'));
 
+    // If localStorage doesn't exist, then there can't be any saved recipes
+    // so set it to false. Otherwise do array some (returns boolean) to see
+    // if recipe is in saved list using object key idMeal value
     const isSaved = !clientStore ? false : clientStore.some((lookUp: any) => lookUp.idMeal === recipe.idMeal);
 
+    // Return object with recipe object and boolean state
     return {
       recipe,
       isSaved
