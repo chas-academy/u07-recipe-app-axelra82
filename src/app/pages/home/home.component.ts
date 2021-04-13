8f565737b6b431735814f4fe97f2afb1d06ddbf7
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 13:24:36 +0200

    chore: setup home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {  ActivatedRoute  } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+
+import * as RecipeActions from '../../actions/recipe.actions';
+import { Recipes } from '../../models/recipes.model';
+
+import { MealdbService } from '../../services/recipes.service';
+
+interface AppState {
+  recipes: Recipes;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +18,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  searchString: string;
+  isSearch: boolean = false;
+  resultCount: number = 0;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<AppState>,
+    private mealdbService:MealdbService,
+  ) { }
 
   ngOnInit(): void {
-  }
 
+    // Subcriber to uri search parameters from route
+    this.route.queryParams.subscribe(params => {
+      // If there is a search set to true
+      this.isSearch = params.search && true;
+      // Extract search string
+      this.searchString = params.search
+      // Make sure we have a string to search
+      if(this.searchString){
+        // Subscribe to mealDB service
+        this.mealdbService.list(this.searchString).subscribe(
+          data => {
+            // Dispatch (send/post) mealDB service result to our global store 
+            this.store.dispatch(new RecipeActions.GetRecipes(data.meals));
+            // Set the result count from the result length
+            this.resultCount = data.meals.length;
+          }
+        );
+      }
+    });
+  }
 }
