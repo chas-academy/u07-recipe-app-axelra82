5125498f0bc7a0064a294ec90fc9872c114b669c
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 16:18:35 +0200

    fix: better env handling with global process env from webpack

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
    production: true,
+   REAPP_MEALDB_API_KEY: process.env.REAPP_MEALDB_API_KEY,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-
 export const environment = {
    production: false,
+   REAPP_MEALDB_API_KEY: process.env.REAPP_MEALDB_API_KEY,
 };
