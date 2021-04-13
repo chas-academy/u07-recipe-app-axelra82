672397916025bfb1b41b1125da6307af7f242588
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-14 00:44:59 +0200

    fix: specify actions

--- src/app/actions/recipe.actions.ts
+++ src/app/actions/recipe.actions.ts
@@ -1,17 +1,28 @@
 import { Action } from '@ngrx/store';
 
 export const RECIPE_LIST = '[Recipes] Get';
-export const RECIPE_FILTER = '[Recipe] Filter';
+export const RECIPE_LIST_FILTER = '[Recipes] Filter';
+export const RECIPE_FILTER_ADD = '[Recipes] Filter Add';
+export const RECIPE_FILTER_REMOVE = '[Recipes] Filter Remove';
 
 export class RecipeList implements Action {
     readonly type = RECIPE_LIST;
-    constructor(public payload: any) {}   
+    constructor(public payload: any) {};
 }
-export class RecipeFilter implements Action {
-    readonly type = RECIPE_FILTER;
-    constructor(public payload: object) {}   
+export class RecipeListFilter implements Action {
+    readonly type = RECIPE_LIST_FILTER;
+}
+export class RecipeFilterAdd implements Action {
+    readonly type = RECIPE_FILTER_ADD;
+    constructor(public payload: string) {};
+}
+export class RecipeFilterRemove implements Action {
+    readonly type = RECIPE_FILTER_REMOVE;
+    constructor(public payload: string) {};
 }
 
 export type All
-    = RecipeFilter |
+    = RecipeFilterRemove |
+    RecipeFilterAdd |
+    RecipeListFilter |
     RecipeList;
