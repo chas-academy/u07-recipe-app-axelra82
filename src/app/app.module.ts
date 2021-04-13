da3d0088819a100d67baf5e3c43832cab14b6441
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 13:04:15 +0200

    chore: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,62 @@
-// Native NG
+// NG
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-// Route
+// Root and route
 import { AppRoutingModule } from './app-routing.module';
-// Components
 import { AppComponent } from './app.component';
-import { ListComponent, SingleComponent } from './components/recipe/recipe.component';
+// Components
 import { NavComponent } from './components/nav/nav.component';
 import { SearchComponent } from './components/search/search.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ListComponent, SingleComponent } from './components/recipe/recipe.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { SaveBtnComponent } from './components/save-btn/save-btn.component';
+// Services
+import { MealdbService } from './services/recipes.service';
+import { SaveBtnService } from './services/save-btn.service';
+// Pipes
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { TagsPipe } from './pipes/tags.pipe';
+import { UnhashPipe } from './pipes/unhash.pipe';
 // Pages
 import { HomeComponent } from './pages/home/home.component';
 import { SavedComponent } from './pages/saved/saved.component';
 // Store (a.k.a state)
 import { StoreModule } from '@ngrx/store';
+import { postReducer } from './reducers/recipe.reducer';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     SearchComponent,
+    FilterComponent,
     ListComponent,
     SingleComponent,
+    TruncatePipe,
+    TagsPipe,
+    UnhashPipe,
+    NavComponent,
     HomeComponent,
-    SavedComponent
+    TagsComponent,
+    SavedComponent,
+    SaveBtnComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    StoreModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ recipes: postReducer }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MealdbService,
+    SaveBtnService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
