1763910f04145dccf1416fc44ed81638e6cf3de8
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 13:39:28 +0200

    chore: add list component

--- src/app/components/recipe/list/list.component.ts
+++ src/app/components/recipe/list/list.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { SaveBtnService } from '../../../services/save-btn.service';
+
+import { Store } from '@ngrx/store';
+
+import { Recipes } from '../../../models/recipes.model';
+
+interface AppState {
+  recipes: Recipes;
+}
 
 @Component({
   selector: 'app-list',
@@ -7,9 +18,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  recipesSubscriber: Observable<Recipes>;
+  recipesData: Recipes['list'];
+  
+  constructor(
+    private store: Store<AppState>,
+    private saveBtnService: SaveBtnService
+  ) {    
+    this.recipesSubscriber = this.store.select('recipes');
   }
 
+  ngOnInit(): void {
+    this.recipesSubscriber.subscribe(
+      data => this.recipesData = data.list
+    )
+  }
+
+  saveBtnData(recipe: object){
+    this.saveBtnService.saveBtnData(recipe);
+  }
 }
