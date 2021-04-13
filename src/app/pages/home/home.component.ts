ff66488b3a116f3c48eb1bc781df100e9ff16556
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-14 00:47:16 +0200

    chore: test filter function

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {  ActivatedRoute  } from '@angular/router';
 
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import * as RecipeActions from '../../actions/recipe.actions';
 import { Recipes } from '../../models/recipes.model';
@@ -19,6 +20,7 @@ interface AppState {
 })
 export class HomeComponent implements OnInit {
   
+  recipesSubscriber: Observable<Recipes>;
   searchString: string;
   isSearch: boolean = false;
   resultCount: number = 0;
@@ -27,7 +29,9 @@ export class HomeComponent implements OnInit {
     private route: ActivatedRoute,
     private store: Store<AppState>,
     private mealdbService:MealdbService,
-  ) { }
+  ) {
+    this.recipesSubscriber = this.store.select('recipes');
+  }
 
   ngOnInit(): void {
 
@@ -46,8 +50,49 @@ export class HomeComponent implements OnInit {
             this.store.dispatch(new RecipeActions.RecipeList(data.meals));
             // Set the result count from the result length
             this.resultCount = data.meals ? data.meals.length : 0;
-          }
-        );
+
+
+            // this.recipesSubscriber.subscribe(
+            //   (data: any) => {
+            //     this.recipesData = data.list;
+            //     if(this.recipesData && this.recipesData.length !== 0){
+            //       this.haveResults = true;
+            //     }
+            //     const filterList = data.filter;
+            //     if(filterList.length){
+            //       console.log(filterList);
+            //       const filteredList = this.recipesData.map((recipe:any) => {
+            //         // Make sure tags has content, if content turn it into array
+            //         const tagsArray = recipe.strTags ? recipe.strTags.toLowerCase().split(',') : [];
+            //         const tagsTest = tagsArray.some((str: string) => filterList.includes(str));
+
+            //         // let categoryTest: boolean;
+            //         let match: boolean = false;
+                    
+            //         filterList.forEach((filter: string) => {
+            //           // tagsTest || categoryTest  
+            //           match = tagsArray.some((str: string) => str === filter) ||
+            //           recipe.strCategory.toLowerCase().includes(filter);  
+            //         });
+
+            //         console.log(match);
+
+            //         // recipe.strCategory.toLowerCase().includes((str:string) => .includes(str));
+
+            //         // console.log(recipe.strCategory);
+                    
+            //         // console.log(categoryTest);
+                    
+            //         // If an test results true for recipe add to filtered list
+            //         if(match){
+            //             return recipe;
+            //         }
+
+            //       }).filter((f: any) => f !== undefined);
+            //       this.recipesData = filteredList;
+            //     }
+            // })
+        })
       }
     });
   }
