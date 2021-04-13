68ccdf76c53996c65dce39b6105819afa7d96fc3
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-13 14:36:57 +0200

    fix: update save button service local storage check

--- src/app/services/save-btn.service.ts
+++ src/app/services/save-btn.service.ts
@@ -10,17 +10,22 @@ export class SaveBtnService {
   saveBtnData(recipe: any){
     // Parse localStorage target 'nomnombase'
     const clientStore = JSON.parse(localStorage.getItem('nomnombase'));
-
-    // If localStorage doesn't exist, then there can't be any saved recipes
+    
+    // If localStorage is null, then there can't be any saved recipes
     // so set it to false. Otherwise do array some (returns boolean) to see
-    // if recipe is in saved list using object key idMeal value
-    const isSaved = !clientStore ? false : clientStore.some((lookUp: any) => lookUp.idMeal === recipe.idMeal);
+    // if recipe is in saved list using object key idMeal value (use && to make sure
+    // lookUp object exists)
+    const isSaved = clientStore === null
+    ? false
+    :
+    clientStore.some(
+      (lookUp: any) => lookUp && lookUp.idMeal === recipe.idMeal
+    );
 
     // Return object with recipe object and boolean state
     return {
+      isSaved,
       recipe,
-      isSaved
     };
   }
 }
-
