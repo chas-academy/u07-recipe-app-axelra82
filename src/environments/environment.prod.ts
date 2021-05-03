69c29991e12548d8ace947477b93cfd36eefd214
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-03 17:50:58 +0200

    chore: add auth api route env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
    production: true,
    REAPP_MEALDB_API_KEY: process.env.REAPP_MEALDB_API_KEY,
+   AUTH_API_URL: process.env.AUTH_API_URL,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
    production: false,
    REAPP_MEALDB_API_KEY: process.env.REAPP_MEALDB_API_KEY,
+   AUTH_API_URL: process.env.AUTH_API_URL,
 };
