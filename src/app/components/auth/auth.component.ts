478ba872414564be98005c005e1fa074970ada3a
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-03 17:49:59 +0200

    chore: update auth components

---
+++ src/app/components/auth/auth.component.ts
@@ -0,0 +1,10 @@
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+export {
+    SignupComponent,
+    SigninComponent,
+    ProfileComponent,
+}
--- src/app/components/auth/profile/profile.component.ts
+++ src/app/components/auth/profile/profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../shared/auth.service';
+
+// User interface
+export class User {
+  name: String;
+  email: String;
+}
 
 @Component({
-  selector: 'app-profile',
+  selector: 'app-user-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
+  UserProfile: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public authService: AuthService
+  ) {
+    this.authService.profileUser().subscribe((data:any) => {
+      this.UserProfile = data;
+    })
   }
+
+  ngOnInit() { }
 
 }
