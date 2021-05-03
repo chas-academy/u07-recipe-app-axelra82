1da15b58175181b1dfaa6e605e6bc70c9e99cca9
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-03 21:17:38 +0200

    chore: whitelist all except auth routes for interceptor

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -8,12 +8,24 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private tokenService: TokenService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const accessToken = this.tokenService.getToken();
-        req = req.clone({
-            setHeaders: {
-                Authorization: "Bearer " + accessToken
-            }
-        });
+        // Array of pages that should use intercepter
+        const authRoutes = [
+            'profile',
+            'login',
+            'logout',
+            'signup',
+        ];
+        // Match auth routes array in request url
+        const inAuthRoute = authRoutes.some(r => req.url.includes(r));
+        // If in auth routes, add auth header        
+        if(inAuthRoute){
+            const accessToken = this.tokenService.getToken();
+            req = req.clone({
+                setHeaders: {
+                    Authorization: "Bearer " + accessToken
+                }
+            });
+        }
         return next.handle(req);
     }
 }
