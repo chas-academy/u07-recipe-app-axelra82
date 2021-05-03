5ff32761672a2cafd4933d4e32c17e1e8ea50d7b
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-03 15:27:49 +0200

    chore: add reactive forms module to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Root and route
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +48,7 @@ import { postReducer } from './reducers/recipe.reducer';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     StoreModule.forRoot({ recipes: postReducer }),
   ],
