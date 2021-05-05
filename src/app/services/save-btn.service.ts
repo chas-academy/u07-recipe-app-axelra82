6b2604dcbdf14011022689411bd8002f4505f022
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-05 21:48:30 +0200

    chore: update save button service

--- src/app/services/save-btn.service.ts
+++ src/app/services/save-btn.service.ts
@@ -5,27 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class SaveBtnService {
 
+  private data: any;
+
   constructor() { }
  
-  saveBtnData(recipe: any){
-    // Parse localStorage target 'nomnombase'
-    const clientStore = JSON.parse(localStorage.getItem('nomnombase'));
-    
-    // If localStorage is null, then there can't be any saved recipes
-    // so set it to false. Otherwise do array some (returns boolean) to see
-    // if recipe is in saved list using object key idMeal value (use && to make sure
-    // lookUp object exists)
-    const isSaved = clientStore === null
-    ? false
-    :
-    clientStore.some(
-      (lookUp: any) => lookUp && lookUp.idMeal === recipe.idMeal
-    );
+  setData(recipe: any){
+    this.data = recipe;
+  }
 
-    // Return object with recipe object and boolean state
-    return {
-      isSaved,
-      recipe,
-    };
+  getData(){
+    return this.data;
   }
 }
