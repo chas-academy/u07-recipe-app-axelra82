e36d0d62720ce522e2f39c8c40c1ec6d1388ad67
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-05 10:26:57 +0200

    chore: rename files

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -1,10 +1,11 @@
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 export {
-    SignupComponent,
     SigninComponent,
+    SignupComponent,
     ProfileComponent,
+    RecipeListComponent,
 }
