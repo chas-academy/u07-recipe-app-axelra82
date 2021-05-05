833431cbd79521fa81664cc9e403db4dbb656108
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-05 09:47:28 +0200

    chore: create pipe collection

---
+++ src/app/pipes/pipes.ts
@@ -0,0 +1,11 @@
+import { TagsPipe } from './tags.pipe';
+import { TruncatePipe } from './truncate.pipe';
+import { UnhashPipe } from './unhash.pipe';
+import { ParsePipe } from './parse.pipe';
+
+export {
+    TagsPipe,
+    TruncatePipe,
+    UnhashPipe,
+    ParsePipe,
+}
