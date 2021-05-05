3d61f93446afa7bb3636f97fd43ae9a80aa0ba28
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-05 21:50:38 +0200

    chore: simplify save button component

--- src/app/components/save-btn/save-btn.component.ts
+++ src/app/components/save-btn/save-btn.component.ts
@@ -1,43 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-save-btn',
   templateUrl: './save-btn.component.html',
   styleUrls: ['./save-btn.component.scss']
 })
-export class SaveBtnComponent implements OnInit {
-
-  @Input() saveBtnInput: any;
-
-  isSaved: boolean;
-  recipe: any;
-  
-  constructor() { }
-
-  ngOnInit(): void {    
-    this.isSaved = this.saveBtnInput.isSaved;
-    this.recipe = this.saveBtnInput.recipe;
-  }
-
-  parsedLocal(){
-    return JSON.parse(localStorage.getItem('nomnombase'));
-  }
-
-  saveToList(){
-    let savedRecipes = this.parsedLocal();
-    if(savedRecipes === null){
-      savedRecipes = [];
-    }
-
-    savedRecipes.push(this.recipe);
-    localStorage.setItem('nomnombase', JSON.stringify(savedRecipes));
-    this.isSaved = true;
-  }
-
-  removeFromList(){
-    let savedRecipes = this.parsedLocal();
-    savedRecipes = savedRecipes.filter((r:any) => r && r.idMeal !== this.recipe.idMeal);
-    localStorage.setItem('nomnombase', JSON.stringify(savedRecipes));
-    this.isSaved = false;
-  }
-}
+export class SaveBtnComponent { }
