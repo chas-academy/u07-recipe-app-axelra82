814742feb8589c388a128c00360d7bb4b436926b
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-05 09:46:52 +0200

    chore: check token validity with now < expiration

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,30 +1,42 @@
 import { Injectable } from "@angular/core";
+import { Router } from '@angular/router';
 import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
 import { TokenService } from "./token.service";
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private tokenService: TokenService) { }
+    constructor(
+        private tokenService: TokenService,
+        public router: Router,
+    ) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        // Array of pages that should use intercepter
+        // Array of routes that should use intercepter
         const authRoutes = [
             'profile',
             'login',
             'logout',
             'signup',
+            'recipelists',
+            'recipelist',
         ];
         // Match auth routes array in request url
         const inAuthRoute = authRoutes.some(r => req.url.includes(r));
         // If in auth routes, add auth header        
         if(inAuthRoute){
             const accessToken = this.tokenService.getToken();
-            req = req.clone({
-                setHeaders: {
-                    Authorization: "Bearer " + accessToken
-                }
-            });
+            const isValidToken = this.tokenService.isValidToken();
+                        
+            if(isValidToken){
+                req = req.clone({
+                    setHeaders: {
+                        Authorization: "Bearer " + accessToken
+                    }
+                });
+            }else{
+                this.router.navigate(['login']);
+            }
         }
         return next.handle(req);
     }
