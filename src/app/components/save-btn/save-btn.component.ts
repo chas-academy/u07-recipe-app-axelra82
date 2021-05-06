f203aee2d8c6830698a7b418ac02fee01ff48891
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-06 10:28:38 +0200

    chore: add logged in check for save button

--- src/app/components/save-btn/save-btn.component.ts
+++ src/app/components/save-btn/save-btn.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { TokenService } from '../../shared/token.service';
 
 @Component({
   selector: 'app-save-btn',
   templateUrl: './save-btn.component.html',
   styleUrls: ['./save-btn.component.scss']
 })
-export class SaveBtnComponent { }
+export class SaveBtnComponent {
+  
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private token: TokenService,
+  ) {
+    this.isLoggedIn = this.token.isLoggedIn();
+  }
+  
+}
